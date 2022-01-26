d76d68c2a478e5bd68e7998708df57f34ccb70c3
Samuel Taylor <git@samueltaylor.org>
2022-01-25 20:44:11 -0600

    Add model search

--- src/rsstool/constants.py
+++ src/rsstool/constants.py
@@ -11,7 +11,7 @@ if DB_LOC is None:
 MODELS_LOC = os.getenv("RSS_MODELS_LOC", None)
 if MODELS_LOC is None:
     parent = pathlib.Path(__file__).parent.parent.parent.absolute()
-    DB_LOC = parent / "models"
+    MODELS_LOC = parent / "models"
 
 USERNAME = os.getenv("RSS_USERNAME", uuid.uuid4())
 PASSWORD = os.getenv("RSS_PASSWORD", uuid.uuid4())
--- src/rsstool/ml.py
+++ src/rsstool/ml.py
@@ -1,8 +1,11 @@
 import sqlite3
 import contextlib
+import pickle
+import os
 import json
 import logging
 import datetime as dt
+import time
 from urllib.parse import urlparse
 
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -12,6 +15,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer, HashingVectorizer
 from sklearn.preprocessing import MultiLabelBinarizer, OneHotEncoder, FunctionTransformer, StandardScaler
 from sklearn.feature_selection import VarianceThreshold, SelectPercentile
+from sklearn.linear_model import LogisticRegression
 
 from sklearn.model_selection import RandomizedSearchCV
 import scipy.stats as st
@@ -48,6 +52,18 @@ def get_training_data(db_loc: str = DB_LOC) -> pd.DataFrame:
     return feed_items
 
 
+class MLBWrapper(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.mlb = MultiLabelBinarizer()
+
+    def fit(self, X, y=None):
+        self.mlb.fit(X)
+        return self
+
+    def transform(self, X):
+        return self.mlb.transform(X)
+
+
 def get_netloc(url):
     if isinstance(url, str):
         return urlparse(url).netloc
@@ -63,11 +79,61 @@ def get_strlen(url):
     return np.log(val).values.reshape(-1, 1)
 
 
-def build_model(feed_id, in_df):
+def build_model(feed_id, in_df, n_iter=1_000, n_jobs=4):
     if (in_df["feed_id"] != feed_id).sum() > 0:
         raise ValueError(f"Expected all feed_id values to be {feed_id}")
     LOG.info(f"Building model for {feed_id}: {in_df.shape[0]} rows")
-    return None
+
+    tr = ColumnTransformer(
+        [
+            ("tfidf", TfidfVectorizer(ngram_range=(1, 2)), "title"),
+            ("hashing", HashingVectorizer(), "title"),
+            ("mlb", MLBWrapper(), "categories"),
+            ("ohe", OneHotEncoder(handle_unknown="ignore"), ["author"]),
+            ("domain", make_pipeline(FunctionTransformer(get_netloc), OneHotEncoder(handle_unknown="ignore")), "link"),
+            ("url_len", FunctionTransformer(get_strlen), "link"),
+            ("title_len", FunctionTransformer(get_strlen), "title"),
+        ],
+        remainder="drop",
+        n_jobs=1,
+        sparse_threshold=0,
+    )
+
+    p = Pipeline(
+        [
+            ("tr", tr),
+            ("scale", StandardScaler()),
+            ("sel_var", VarianceThreshold(threshold=1e-3)),
+            ("sel_fcl", SelectPercentile(percentile=10)),
+            ("clf", LogisticRegression(n_jobs=2, solver="saga", penalty="elasticnet", tol=1e-3, max_iter=100_000)),
+        ]
+    )
+
+    search = RandomizedSearchCV(
+        p,
+        {
+            "tr__tfidf__ngram_range": [(1, 1), (1, 2)],
+            "tr__hashing__n_features": st.randint(10, 300),
+            "tr__hashing__norm": ["l1", "l2"],
+            "tr__hashing__binary": [True, False],
+            "sel_var__threshold": st.uniform(0, 1),
+            "sel_fcl__percentile": st.randint(10, 90),
+            "clf__C": st.uniform(0.1, 0.99),
+            "clf__l1_ratio": st.uniform(0, 1),
+            "clf__class_weight": [None, "balanced"],
+        },
+        scoring="roc_auc",
+        cv=5,
+        n_jobs=n_jobs,
+        n_iter=n_iter,
+    )
+
+    search.fit(in_df, in_df["has_clicks"])
+
+    LOG.info(f"{feed_id} params: {search.best_params_}")
+    LOG.info(f"{feed_id} score: {search.best_score_:.03f}")
+
+    return search
 
 
 if __name__ == "__main__":
@@ -85,7 +151,7 @@ if __name__ == "__main__":
         in_df = feed_items.query("feed_id == @feed_id")
 
         start_ctr = time.perf_counter()
-        model = build_model(feed_id, in_df)
+        model = build_model(feed_id, in_df, n_iter=4)
         meta = {
             "train_start": start_time,
             "train_duration": time.perf_counter() - start_ctr,
@@ -93,8 +159,8 @@ if __name__ == "__main__":
             "n_rows": in_df.shape[0],
             "n_positives": in_df["has_clicks"].sum(),
             "nonzero_coef_ct": 0,  # TODO
-            "best_params": {},  # TODO
-            "best_score": 0.001,  # TODO
+            "best_params": model.best_params_,
+            "best_score": model.best_score_,
         }
-        with open(MODELS_LOC / f"{feed_id}.pkl", "wb") as f:
-            f.write({"model": model, "meta": meta})
+        with open(os.path.join(MODELS_LOC, f"{feed_id}.pkl"), "wb") as f:
+            pickle.dump({"model": model, "meta": meta}, f)
